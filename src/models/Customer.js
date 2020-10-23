c88cbac4f87a3d4a874c73a48d0298fe2ac47b4d
erickk474 <erickk474@gmail.com>
2020-10-22 23:02:13 -0200

    update

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -174,6 +174,7 @@ class Customer {
     async updatePassword(request) {
         try {
             const { body, user } = request;
+            console.log({ body, user })
 
             if (!body.new_password) {
                 this.setResponse({ message: 'Please, fill in all required fields' }, 400);
